f13628af15c7283a2982961f4a0c4d152f9ce4cc
Roma <krakenhri@mail.ru>
2024-12-02 18:39:59 +0300

    feat: completed task

--- src/task.tsx
+++ src/task.tsx
@@ -1,5 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
+import {
+	FC,
+	memo,
+	MouseEvent,
+	useCallback,
+	useEffect,
+	useRef,
+	useState,
+} from 'react'
 
 type Mode = 'add' | 'remove'
 
@@ -10,117 +17,166 @@ const useRenderCounter = () => {
 	return counter.current
 }
 
-const RenderCountLabel = ({
-	count,
-	label,
-}: {
+type RenderCountProps = {
 	count: number
 	label: string
-}) => {
+}
+
+const RenderCountLabel: FC<RenderCountProps> = memo(({ count, label }) => {
 	return (
 		<div>
 			{label} renders: <span style={{ color: 'red' }}>{count}</span>
 		</div>
 	)
-}
+})
 
-const AddToEndButton = ({ onClick }: { onClick: () => void }) => {
-	const counter = useRenderCounter()
-	return (
-		<div className='button'>
-			<button onClick={onClick}>Add to end</button>
-			<RenderCountLabel label='Button' count={counter} />
-		</div>
-	)
-}
+// const AddToEndButton = ({ onClick }: { onClick: () => void }) => {
+// 	const counter = useRenderCounter()
+// 	return (
+// 		<div className='button'>
+// 			<button onClick={onClick}>Add to end</button>
+// 			<RenderCountLabel label='Button' count={counter} />
+// 		</div>
+// 	)
+// }
 
-const AddToStartButton = ({ onClick }: { onClick: () => void }) => {
-	const counter = useRenderCounter()
-	return (
-		<div>
-			<button onClick={onClick}>Add to start</button>
-			<RenderCountLabel label='Button' count={counter} />
-		</div>
-	)
-}
+// const AddToStartButton = ({ onClick }: { onClick: () => void }) => {
+// 	const counter = useRenderCounter()
+// 	return (
+// 		<div>
+// 			<button onClick={onClick}>Add to start</button>
+// 			<RenderCountLabel label='Button' count={counter} />
+// 		</div>
+// 	)
+// }
 
-const ChangeModeButton = ({
-	action,
-	onClick,
-}: {
-	action: Mode
+// const ChangeModeButton = ({
+// 	action,
+// 	onClick,
+// }: {
+// 	action: Mode
+// 	onClick: () => void
+// }) => {
+// 	const counter = useRenderCounter()
+// 	return (
+// 		<div>
+// 			<button onClick={onClick}>change mode: {action}</button>
+// 			<RenderCountLabel label='Button' count={counter} />
+// 		</div>
+// 	)
+// }
+
+type CustomBtnProps = {
+	title: string
 	onClick: () => void
-}) => {
+}
+
+const CustomButton: FC<CustomBtnProps> = memo(({ title, onClick }) => {
 	const counter = useRenderCounter()
 	return (
 		<div>
-			<button onClick={onClick}>change mode: {action}</button>
+			<button onClick={onClick}>{title}</button>
 			<RenderCountLabel label='Button' count={counter} />
 		</div>
 	)
-}
+})
 
-const ListItem = ({
-	item,
-	onRemove,
-}: {
+type ListItemProps = {
 	item: string
 	onRemove: (item: string) => void
-}) => {
-	const onClick = () => {}
+}
+
+const ListItem: FC<ListItemProps> = ({ item, onRemove }) => {
+	const onClick = (event: MouseEvent<HTMLLIElement>) => {
+		event.currentTarget.classList.remove('li-item')
+		void event.currentTarget.offsetWidth
+		event.currentTarget.classList.add('li-item')
+	}
+
+	const remove = (e: MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation()
+		onRemove(item)
+	}
+
 	return (
 		<li onClick={onClick} className='li-item'>
 			{item}
-			<button className='btn-remove' onClick={() => onRemove(item)}>
+			<button className='btn-remove' onClick={remove}>
 				x
 			</button>
 		</li>
 	)
 }
 
-let index = 0
-
 const List = () => {
 	const counter = useRenderCounter()
+	const index = useRef(0)
+
 	const [items, setItems] = useState<string[]>([])
 	const [action, setAction] = useState<Mode>('add')
 
-	const handleChangeAction = () => {
+	const addItem = useCallback((place: 'start' | 'end') => {
+		index.current++
+		switch (place) {
+			case 'start':
+				setItems(prev => [`${index.current}-item`, ...prev])
+
+				break
+			case 'end':
+				setItems(prev => [...prev, `${index.current}-item`])
+
+				break
+		}
+	}, [])
+
+	const handleChangeAction = useCallback(() => {
 		setAction(prev => (prev === 'add' ? 'remove' : 'add'))
-	}
+	}, [])
 
-	const handlRemoveItems = () => {
-		setItems(prev => prev.slice(0, prev.length - 1))
-	}
+	const handleRemoveItem = useCallback((item?: string) => {
+		if (item) {
+			setItems(prev => prev.filter(i => i !== item))
+		} else {
+			setItems(prev =>
+				prev.length > 0 ? prev.slice(0, prev.length - 1) : prev
+			)
+		}
+	}, [])
 
-	const handleRemoveItem = () => {}
+	const handleAddItem = useCallback(() => {
+		addItem('end')
+	}, [addItem])
 
-	const handleAddItem = () => {
-		index++
-		setItems(prev => [...prev, `${index}-item`])
-	}
-
-	const handleAddToStart = () => {}
+	const handleAddToStart = useCallback(() => {
+		addItem('start')
+	}, [addItem])
 
 	useEffect(() => {
-		setTimeout(
-			() => (action === 'add' ? handleAddItem() : handlRemoveItems()),
+		const id = setTimeout(
+			() => (action === 'add' ? handleAddItem() : handleRemoveItem()),
 			1000
 		)
+
+		return () => {
+			clearTimeout(id)
+		}
 	})
 
 	return (
 		<ul className='list'>
 			<RenderCountLabel label='List' count={counter} />
 			<br />
-			<ChangeModeButton action={action} onClick={handleChangeAction} />
+			<CustomButton
+				title={`change mode: ${action}`}
+				onClick={handleChangeAction}
+			/>
 			<br />
 			<div className='btn-actions'>
-				<AddToStartButton onClick={handleAddToStart} />
-				<AddToEndButton onClick={handleAddItem} />
+				<CustomButton title={'Add to start'} onClick={handleAddToStart} />
+				<CustomButton title={'Add to end'} onClick={handleAddItem} />
 			</div>
 			{items.map(item => (
-				<ListItem item={item} onRemove={handleRemoveItem} />
+				<ListItem item={item} onRemove={handleRemoveItem} key={item} />
 			))}
 		</ul>
 	)
